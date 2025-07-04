fa28f9d583e84993c09c9244a919ade6a3a91423
Matt Franceschini <franceschinimatt@gmail.com>
2025-07-04 12:35:46 -0400

    update

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+/**
+ * @filename: lint-staged.config.js
+ * @type {import('lint-staged').Configuration}
+ */
+export default {
+  'src/**/*.{js,jsx,ts,tsx}': [
+      `prettier --write`,
+      `eslint --fix`,
+    ],
+}
--- src/lib/routes/code.ts
+++ src/lib/routes/code.ts
@@ -541,6 +541,7 @@ export function codeRoutes(app: Express) {
 
       const response = await compareBranchToDefaultBranch(
         user.accessToken,
+
         `https://github.com/${repo.fullName}`,
         branch
       );
