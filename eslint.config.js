ffd7a9f360be83ac7041819a50fb8b0578da91d5
Matt Franceschini <franceschinimatt@gmail.com>
2025-09-18 12:22:59 -0400

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,21 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import prettierPlugin from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier";
+import unusedImportsPlugin from "eslint-plugin-unused-imports"
 
-export default tseslint.config(
+export default [
+  // Global ignores
+  {
+    ignores: ["node_modules/**", "dist/**"],
+  },
+
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   prettierConfig,
   {
     plugins: {
       prettier: prettierPlugin,
+      "unused-imports": unusedImportsPlugin,
     },
     rules: {
       "prettier/prettier": "error",
@@ -18,6 +25,29 @@ export default tseslint.config(
         "error",
         { argsIgnorePattern: "^_" },
       ],
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "react/jsx-tag-spacing": [
+        "error",
+        {
+          closingSlash: "never",
+          beforeSelfClosing: "always",
+          afterOpening: "never",
+          beforeClosing: "never",
+        },
+      ],
+      "react/jsx-indent": ["error", 2],
+      "react/jsx-indent-props": ["error", 2],
+      "react/jsx-curly-spacing": ["error", { when: "never", children: true }],
+      "react/jsx-equals-spacing": ["error", "never"],
     },
     languageOptions: {
       ecmaVersion: "latest",
@@ -35,6 +65,6 @@ export default tseslint.config(
   },
   {
     // Ignore dist directory and config files
-    ignores: ["dist/**", "*.config.js", ".lintstagedrc.js", "drizzle.config.ts"]
+    ignores: ["dist/**", "*.config.js", "drizzle.config.ts"]
   }
-);
+]
